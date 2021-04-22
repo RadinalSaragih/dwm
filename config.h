556030b17c9f80b5f6cbb1fb85d5f7ab1c2ad110
RadinalSaragih <radinalsaragih13@protonmail.com>
2021-04-22 15:33:22 +0700

    Removed a Patch

--- config.h
+++ config.h
@@ -173,12 +173,6 @@ static Key keys[] = {
   { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
   { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 
-  { MODKEY|ControlMask,           XK_h,      viewtoleft,     {0} },
-  { MODKEY|ControlMask,           XK_l,      viewtoright,    {0} },
-
-  { MODKEY|ShiftMask,             XK_h,      tagtoleft,      {0} },
-  { MODKEY|ShiftMask,             XK_l,      tagtoright,     {0} },
-
   { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
   { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
   { MODKEY|ControlMask,           XK_comma,  tagmon,         {.i = -1 } },
