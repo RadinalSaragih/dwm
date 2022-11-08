59ec893f9f97310624d2ffb8d798e711be879fad
Radinal Saragih <rss@radinalsaragih@gmail.com>
2022-11-09 03:28:03 +0700

    added installation path for regular user, and made dwm.desktop installation optional

--- config.mk
+++ config.mk
@@ -4,9 +4,10 @@ VERSION = 6.4
 # Customize below to fit your system
 
 # paths
+# PREFIX = ${HOME}/.local
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
-XSESSIONS = /usr/share/xsessions
+# XSESSIONS = /usr/share/xsessions
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
